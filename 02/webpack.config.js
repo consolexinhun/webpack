efb1f6e490d6c9fd8a72a9c23b4102ec1bdd2fc3
consolexin <consolexin@gmail.com>
2020-11-23 20:15:18 +0800

    k

--- 02/webpack.config.js
+++ 02/webpack.config.js
@@ -1,4 +1,13 @@
+let path = require("path") //获取当前的目录
+
 module.exports = {
     // 入口文件
-    
+    entry: "./src/index.js",
+    output: {
+        // 输出文件名称
+        filename: "bundle.js",
+        // 输出的路径，绝对路径
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: "development"  //or `production`
 }

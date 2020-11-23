8d4f99b42a0adae1addf3b130de528df0772b7c1
consolexin <consolexin@gmail.com>
2020-11-23 22:01:28 +0800

    o

---
+++ 06/src/index.js
@@ -0,0 +1,10 @@
+import data from "./data.json"
+import "./style.css"
+
+function fn1(){
+    console.log("fn1")
+}
+
+fn1()
+
+console.log(data)
---
+++ 06/webpack.config.js
@@ -0,0 +1,36 @@
+let path = require("path") //获取当前的目录
+
+module.exports = {
+    // 入口文件
+    entry: "./src/index.js",
+    output: {
+        // 输出文件名称
+        filename: "bundle.js",
+        // 输出的路径，绝对路径
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: "development",  //or `production`
+    // loader的配置
+    module: {
+        // 对某个格式的文件进行转换处理
+        rules: [
+            {
+                test: /\.css$/,  //正则
+                use: [
+                    // use数组中的loader顺序为从下到上执行
+                    "style-loader", //将js样式内容插入到style标签里
+                    "css-loader"    //将css文件转为js
+                ]
+            }
+        ]
+    },
+    // 
+    devServer: {
+        // 项目构建路径
+        contentBase: path.resolve(__dirname, "dist"),
+        // 启动gzip压缩
+        compress: true,
+        port: 3000,
+        open: true
+    }
+}

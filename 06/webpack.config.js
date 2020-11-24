41410f7e00f4b5ff75a0d3dbd3a6418020eae65b
consolexin <consolexin@gmail.com>
2020-11-24 11:03:44 +0800

    t

--- 06/webpack.config.js
+++ 06/webpack.config.js
@@ -1,4 +1,5 @@
 let path = require("path") //获取当前的目录
+let HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     // 入口文件
@@ -21,16 +22,37 @@ module.exports = {
                     "style-loader", //将js样式内容插入到style标签里
                     "css-loader"    //将css文件转为js
                 ]
+            },{
+                // 图片文件
+                test: /\.(jpg|png|gif)$/,
+                loader: "url-loader",
+                // 图片小于8k，base64处理，减少请求数量，会使得体积更大
+                options:{
+                    limit: 8*1024, //8k
+                    esModule: false, //关闭ES6解析
+                    name: '[hash:10].[ext]' //取图片哈希的前10位和扩展名
+                }
+            },{
+                // 处理HTML
+                test: /\.html$/,
+                loader: "html-loader"
             }
         ]
     },
+
+    // plugin配置
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ],
     // 
     devServer: {
         // 项目构建路径
         contentBase: path.resolve(__dirname, "dist"),
-        // 启动gzip压缩
+        // 启动编码压缩
         compress: true,
         port: 3000,
-        open: true
+        open: true //自动打开浏览器
     }
 }
